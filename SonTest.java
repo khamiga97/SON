bf4f0360a283685b3b3696978b98263887581be9
Kamil <ilovewisla@gmail.com>
2021-07-14 21:59:06 +0200

    Assert added

--- SonTest.java
+++ SonTest.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -9,7 +10,7 @@ import org.testng.annotations.Test;
 public class SonTest {
 
     @Test
-    public void logIn()  {
+    public void logIn() throws InterruptedException {
 
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
@@ -23,6 +24,14 @@ public class SonTest {
 
         By logInButton = By.cssSelector("[type='submit']");
         driver.findElement(logInButton).click();
+
+        Thread.sleep(1000);
+
+        WebElement result = driver.findElement(By.linkText("Strona główna"));
+
+        Assert.assertTrue(result.isDisplayed());
+
+
 
     }
 }
